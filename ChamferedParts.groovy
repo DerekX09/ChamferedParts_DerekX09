34ff3356080a9af1a4d92410d87e5712d622e93b
hephaestus <hephaestus@zoomtop>
2019-06-01 21:15:53 -0400

    chamfered cube update

--- ChamferedParts.groovy
+++ ChamferedParts.groovy
@@ -1,14 +1,1 @@
-
-return [new ChamferedCube(5,5,5,1.5).toCSG(),
-new ChamferedCylinder(5,5,1).toCSG().movex(10),
-new Wedge(10,5,20).toCSG().movey(10),
-new Isosceles(10,5,20).toCSG().movey(20),
-new Fillet(5,10).toCSG().movey(30),
- Parabola.extrudeByEquation(5,-0.27,0,1)
-	.move(20,20,0),
-Parabola.coneByEquation(5,1.27,0.7)
-	.move(30,30,0),
-	Parabola.coneByFocalLength(10, 10),
-	Parabola.coneByHeight(10, 20
-	).rotx(90).toZMin(),
-]
+new ChamferedCube(20,10,5,1.5).toCSG()
